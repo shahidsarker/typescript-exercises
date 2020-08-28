f927eb3cde6f1b925f702b2e41c267d9ee3b5517
Shahid <shahid.sarker@gmail.com>
2020-08-28 02:23:34 -0400

    calculate bmi

---
+++ bmiCalculator.ts
@@ -0,0 +1,34 @@
+interface BmiValues {
+  value1: number;
+  value2: number;
+}
+
+const parseArguments = (args: Array<string>): BmiValues => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+  if (args.length > 4) throw new Error("too many arguments");
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      value1: Number(args[2]),
+      value2: Number(args[3]),
+    };
+  } else {
+    throw new Error("Provided values were not numbers!");
+  }
+};
+
+const calculateBmi = (height: number, weight: number): string => {
+  const bmi = weight / ((height * height) / 10000);
+  console.log(bmi);
+  if (bmi < 18.5) return "Underweight";
+  else if (bmi < 25) return "Normal weight";
+  else if (bmi < 30) return "Overweight";
+  else return "Obese";
+};
+
+try {
+  const { value1, value2 } = parseArguments(process.argv);
+  console.log(calculateBmi(value1, value2));
+} catch (e) {
+  console.log("error, something bad happened, message: ", e.message);
+}
