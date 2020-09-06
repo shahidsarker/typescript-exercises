3dac3e8adb5caee6c4072cbba8e2c09158626e87
Shahid <shahid.sarker@gmail.com>
2020-09-05 22:12:30 -0400

    errors for bmi path

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -32,4 +32,4 @@ try {
   console.log("error, something bad happened, message: ", e.message);
 }
 
-export {};
+export { BmiValues, calculateBmi };
--- index.ts
+++ index.ts
@@ -1,8 +1,22 @@
 import express from "express";
+import { calculateBmi } from "./bmiCalculator";
 const app = express();
+
+// import { calculateBmi, BmiValues } from "./bmiCalculator";
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!");
+});
+
+app.get("/bmi", (req, res) => {
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
+  const bmi = calculateBmi(height, weight);
+  if (isNaN(height) || isNaN(weight))
+    res.status(400).send({
+      error: "malformatted parameters",
+    });
+  else res.send({ height, weight, bmi });
 });
 
 const PORT = process.env.PORT || 3003;
